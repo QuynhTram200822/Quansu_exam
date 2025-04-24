c9b7abe1155e500cd69b24ce716fdf3a0a301da2
QuynhTram200822 <ttqtram.20it3@vku.udn.vn>
2025-04-24 11:21:45 +0700

    fixbug for language button and padding for footer

--- FE/assets/js/getlink.js
+++ FE/assets/js/getlink.js
@@ -7,14 +7,6 @@ $('.news-item').click(function () {
   return false;
 });
 
-$('.notify-item').click(function () {
-  if ($(this).find('a').attr('target') == '_blank') {
-    window.open($(this).find('a').attr('href'), '_blank');
-  } else {
-    window.location = $(this).find('a').attr('href');
-  }
-  return false;
-});
 $('.news-card__item').click(function () {
   if ($(this).find('a').attr('target') == '_blank') {
     window.open($(this).find('a').attr('href'), '_blank');
